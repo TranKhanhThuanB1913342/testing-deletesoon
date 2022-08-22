cf52bdb2a3f523a36e5dec8b9f600327b356428a
Thuận Trần <thuanlpst2001@gmail.com>
2022-08-22 10:54:03 +0700

    ViewEngine
    #6 - Hoi Dan IT

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+//import express from "express"
 const app = express()
 const port = 3000
+const path = require('path');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app.get('/about', (req, res) => {
